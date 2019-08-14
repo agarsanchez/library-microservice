467c65a3cfbcbdbde4caa98e6bea826c3b205a1e
Alberto Garcia <albertogarcia1984@gmail.com>
2019-08-14 12:22:42 +0100

    Scripts: gitconfigure

---
+++ scripts/gitconfigure.sh
@@ -0,0 +1,2 @@
+git config --global core.editor "vim"
+git config --global alias.lola "log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --date=short"
